33ea1e55c78ee8480906c221b0c86a9077692b37
Anderson Diego José de Oliveira <anderson.jose@inovaipda.org.br>
2024-08-13 23:50:47 -0300

    refactoring to fit on json model as documentation requires.

--- Domain/Models/OrderItemOutput.cs
+++ Domain/Models/OrderItemOutput.cs
@@ -4,16 +4,26 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Domain.Models
 {
     public class OrderItemOutput
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+
+        [JsonPropertyName("idProduto")]
         public int ProductId { get; set; }
+
+        [JsonPropertyName("nomeProduto")]
         public string? ProductName { get; set; }
+
+        [JsonPropertyName("valorUnitario")]
         public double ProductPrice { get; set; }
+
+        [JsonPropertyName("quantidade")]
         public int Quantity { get; set; }
 
         public OrderItemOutput(int id, int productId, string? productName, double productPrice, int quantity)
--- Domain/Models/OrderOutput.cs
+++ Domain/Models/OrderOutput.cs
@@ -1,12 +1,24 @@
-﻿namespace Domain.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.Models
 {
     public class OrderOutput
     {
         public int Id { get; set; }
+
+        [JsonPropertyName("nomeCliente")]
         public string? ClientName { get; set; }
+
+        [JsonPropertyName("emailCliente")]
         public string? ClientEmail { get; set; }
+
+        [JsonPropertyName("pago")]
         public bool PaymentStatus { get; set; }
+
+        [JsonPropertyName("valorTotal")]
         public double TotalPrice { get; set; }
+
+        [JsonPropertyName("itensPedido")]
         public List<OrderItemOutput> OrderItems { get; set; }
 
         public OrderOutput()
